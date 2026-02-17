0cebd377e171c1f1414bf576335fe08d6646e473
Bastian Bense <bb@neo.de>
2026-02-17 13:59:21 +0100

    a

---
+++ projects/nde-components/src/lib/spark-card/spark-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spark-card',
+  imports: [],
+  templateUrl: './spark-card.component.html',
+  styleUrl: './spark-card.component.scss',
+})
+export class SparkCardComponent {
+
+}
